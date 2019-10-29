579818950116c1c39402b196a263987abbd7e999
Cheow Yeu Tyng <kecy6cyt@nottingham.edu.my>
2019-10-29 19:46:37 +0800

    [Research] Updated webcam face detection code with complete face feature detection

--- Research/dlibTest/face_detection_webcam.py
+++ Research/dlibTest/face_detection_webcam.py
@@ -1,6 +1,11 @@
+'''
+For this script, please specifically use [shape_predictor_68_face_landmarks.dat] for trained model
+'''
+
 import sys
 import cv2
 import dlib
+
 
 class Camera:
     def __init__(self, mode):
@@ -13,6 +18,7 @@ class Camera:
 
             # Initialize camera with webcam
             self.camera_object = cv2.VideoCapture(0)
+
             if self.camera_object.isOpened():
                 self.is_camera_open, frame = self.camera_object.read()
             else:
@@ -37,17 +43,35 @@ class Camera:
         # Grab a frame from the camera, and return as image
         if self.mode == "WEB_CAM":
             self.is_camera_open, image = self.camera_object.read()
-            return image
+            return cv2.cvtColor(image, cv2.COLOR_BGR2RGB) 
         elif self.mode == "PI_CAM":
             raise Exception("Not implemented")
 
-# predictor_path = sys.argv[1]
+predictor_path = sys.argv[1]
+
+if len(sys.argv) < 1:
+    print("[Init] Please include trained model")
+    exit()
 
 window = dlib.image_window()
 window.set_title("Test Video")
 
+detector = dlib.get_frontal_face_detector()
+shape_predictor = dlib.shape_predictor(predictor_path)
+
 camera = Camera("WEB_CAM")
 while True:
     image = camera.take_frame()
+
+    dets = detector(image)
+
     window.clear_overlay()
+
+    for k, d in enumerate(dets):
+        shape = shape_predictor(image, d)
+        window.add_overlay(shape)
+
+    
+    window.add_overlay(dets)
+
     window.set_image(image)
