9fcf265d37563daa0566f2585a09a163ca170f07
Cheow Yeu Tyng <kecy6cyt@nottingham.edu.my>
2019-10-30 15:43:40 +0800

    [Research] Added graph plotting to show the Eye Aspect Ratio

--- Research/dlibTest/eyelid_detector.py
+++ Research/dlibTest/eyelid_detector.py
@@ -4,6 +4,7 @@ import dlib
 import numpy
 import matplotlib.pyplot as plt
 from imutils import face_utils
+import matplotlib.pyplot as plt
 
 from utils.Camera import Camera, CameraType
 
@@ -32,6 +33,27 @@ def eye_aspect_ratio(eye):
     return ear
 
 
+plt.style.use("ggplot")
+
+def live_plotter(x_vec, y1_data, line1, identifier='', pause_time=0.1):
+    if line1 == []:
+        plt.ion()
+        fig = plt.figure(figsize=(13, 6))
+        ax = fig.add_subplot(111)
+        line1, = ax.plot(x_vec, y1_data, '-o', alpha=0.8)
+        plt.ylabel("Y Label")
+        plt.title("Title: {}".format(identifier))
+        plt.show()
+
+    line1.set_ydata(y1_data)
+
+    if numpy.min(y1_data) <= line1.axes.get_ylim()[0] or numpy.max(y1_data) >= line1.axes.get_ylim()[1]:
+        plt.ylim([numpy.min(y1_data) - numpy.std(y1_data), numpy.max(y1_data) + numpy.std(y1_data)])
+
+    plt.pause(pause_time)
+
+    return line1
+
 predictor_path = sys.argv[1]
 
 camera = Camera(CameraType.WEB_CAM)
@@ -40,6 +62,10 @@ predictor = dlib.shape_predictor(predictor_path)
 
 window = dlib.image_window()
 window.set_title("Face Detector")
+
+line1 = []
+x_vec = numpy.linspace(0, 1, 101)[0 : -1]
+y_vec = [0] * len(x_vec)
 
 while True:
     frame = camera.take_frame()
@@ -71,9 +97,15 @@ while True:
         # print("Left EAR: {0:.3f} Right EAR: {0:.3f} Average EAR: {0:.3f}".format(
         #     leftEAR, rightEAR, average_EAR))
 
-        value = int(average_EAR * 100)
-        for i in range(value):
-            print("*", end='')
-        print("")
+        # Plot EAR using console
+        # value = int(average_EAR * 100)
+        # for i in range(value):
+        #     print("*", end='')
+        # print("")
+
+        # Plot EAR using matplotlib
+        y_vec[-1] = average_EAR
+        line1 = live_plotter(x_vec, y_vec, line1)
+        y_vec = numpy.append(y_vec[1:], 0.0)
 
             
