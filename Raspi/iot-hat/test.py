2b9dbb9385191be110f471c97abd3ef62359b8ff
Cheow Yeu Tyng <kecy6cyt@nottingham.edu.my>
2019-12-05 11:33:20 +0800

    Added movement detection excluding the face

--- Raspi/iot-hat/test.py
+++ Raspi/iot-hat/test.py
@@ -8,3 +8,4 @@ tcpHelper = TCPHelper(serial, "35.234.201.162", 8200, "celcom2g")
 
 tcpHelper.initializeDevice()
 tcpHelper.sendMessage("I'm skipping my class later!")
+tcpHelper.sendMessage("WTHHHHHHHH")
--- Research/orbTest/detectmovement.py
+++ Research/orbTest/detectmovement.py
@@ -1,4 +1,6 @@
 from utils.Camera import Camera, CameraType
+from utils.GraphPlotter import GraphPlotter
+
 import numpy as np
 import dlib
 import cv2
@@ -10,24 +12,40 @@ import math
 camera = Camera(CameraType.WEB_CAM)
 orb = cv2.ORB_create()
 bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+faceDetector = dlib.get_frontal_face_detector()
+
 imageWindow = dlib.image_window()
+gp = GraphPlotter(yRange=[0, 100])
+gp.add_plot("dmatch-mean")
+gp.add_plot("real-mean")
 
 previousImage = camera.take_frame()
+previousDetectedFaceList = faceDetector(previousImage)
 
 while True:
 
     currentImage = camera.take_frame()
+    currentDetectedFaceList = faceDetector(currentImage)
 
     kp1, des1 = orb.detectAndCompute(previousImage, None)
     kp2, des2 = orb.detectAndCompute(currentImage, None)
 
-    matches = bf.match(des1, des2)
+    try:
+        matches = bf.match(des1, des2)
+    except:
+        print("Something happened...")
+        continue
 
     matches = sorted(matches, key=lambda x:x.distance)
 
     outputImage = currentImage
 
+    dmatchDistanceList = []
+    realDistanceList = []
+
     for match in matches:
+        
         image1KeypointId = match.queryIdx
         image2KeypointId = match.trainIdx
 
@@ -37,18 +55,51 @@ while True:
         image1Coordinate = image1Keypoint.pt
         image2Coordinate = image2Keypoint.pt
 
+        includeKeypoint = True
+
+        for faceIndex, faceBoundingBox in enumerate(previousDetectedFaceList):
+            if (
+                (image1Coordinate[0] > faceBoundingBox.left()) and 
+                (image1Coordinate[0] < faceBoundingBox.right()) and
+                (image1Coordinate[1] > faceBoundingBox.top()) and
+                (image1Coordinate[1] < faceBoundingBox.bottom())):
+                includeKeypoint = False
+
+        for faceIndex, faceBoundingBox in enumerate(currentDetectedFaceList):
+            if (
+                (image2Coordinate[0] > faceBoundingBox.left()) and 
+                (image2Coordinate[0] < faceBoundingBox.right()) and
+                (image2Coordinate[1] > faceBoundingBox.top()) and
+                (image2Coordinate[1] < faceBoundingBox.bottom())):
+                includeKeypoint = False
+
+
+        if includeKeypoint == False:
+            continue
+
         xDiff = abs(image1Coordinate[0] - image2Coordinate[0])
         yDiff = abs(image1Coordinate[1] - image2Coordinate[1])
 
         dist = math.sqrt((xDiff * xDiff) + (yDiff * yDiff))
 
-        print("{}:{}".format(match.distance, dist))
+        if dist > 100:
+            continue
 
-        # print("Coordinate --> Image1: [{}, {}], Image2: [{}, {}]".format(image1Coordinate[0], image1Coordinate[1], image2Coordinate[0], image2Coordinate[1]))
+        dmatchDistanceList.append(match.distance)
+        realDistanceList.append(dist)
 
         cv2.line(outputImage, (int(image1Coordinate[0]), int(image1Coordinate[1])), (int(image2Coordinate[0]), int(image2Coordinate[1])), (0, 255, 0), 1)
 
 
+    # print("DMatch mean: {}, Real mean: {}".format(np.mean(dmatchDistanceList), np.mean(realDistanceList)))
+    gp.input_value("dmatch-mean", np.mean(dmatchDistanceList))
+    gp.input_value("real-mean", np.mean(realDistanceList))
+
+    imageWindow.clear_overlay()
+
     imageWindow.set_image(outputImage)
+    imageWindow.add_overlay(currentDetectedFaceList)
+    imageWindow.add_overlay(previousDetectedFaceList)
 
     previousImage = currentImage
+    previousDetectedFaceList = currentDetectedFaceList
