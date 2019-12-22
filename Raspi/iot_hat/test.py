369b7cf0a1bd8ec8a30116b9aa0e007d8cf6b4b4
Eagle Cheow <eagle.cheow@hotmail.com>
2019-12-22 21:15:02 +0800

    Added server TCP code

--- Raspi/iot_hat/test.py
+++ Raspi/iot_hat/test.py
@@ -1,14 +1,14 @@
-from iot_hat.utils.GPSHelper import GPSObject, GPSHelper
-from iot_hat.utils.SerialHelper import SerialHelper
-from iot_hat.utils.TCPHelper import TCPHelper
+from utils.GPSHelper import GPSObject, GPSHelper
+from utils.SerialHelper import SerialHelper
+from utils.TCPHelper import TCPHelper
 
 serial = SerialHelper("COM6")
 gpsHelper = GPSHelper(serial)
-# tcpHelper = TCPHelper(serial, "35.234.201.162", 8200, "celcom2g")
+tcpHelper = TCPHelper(serial, "35.234.201.162", 8200, "celcom2g")
 
-location = gpsHelper.getGPSLocation()
-print("Coordinate: {}{}, {}{}".format(location.latitude, location.latitudeDirection, location.longitude, location.longitudeDirection))
+# location = gpsHelper.getGPSLocation()
+# print("Coordinate: {}{}, {}{}".format(location.latitude, location.latitudeDirection, location.longitude, location.longitudeDirection))
 
-# tcpHelper.initializeDevice()
-# tcpHelper.sendMessage("This is a test message")
-# tcpHelper.sendMessage("Sending message from SIM7000E")
+tcpHelper.initializeDevice()
+tcpHelper.sendMessage("This is a test message")
+tcpHelper.sendMessage("Sending message from SIM7000E")
